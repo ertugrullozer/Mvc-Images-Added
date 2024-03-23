e2dd34e85e4a2849482d94f4f9fbe1ed27743a51
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-23 13:30:33 +0300

    Proje dosyası ekle.

---
+++ Mvc-Images-Added/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Mvc_Images_Added.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Images_Added.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DbPicturesAddEntities2 db = new DbPicturesAddEntities2();
+        public ActionResult Index()
+        {
+            var List = db.Table_Pictures.ToList();
+            return View(List);
+        }
+
+        [HttpGet]
+        public ActionResult AddImages()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddImages([Bind(Include = "ID,Pictures")] Table_Pictures p, HttpPostedFileBase fileBase)
+        {
+            if (fileBase != null)
+            {
+                string filePath = Path.GetFileName(fileBase.FileName);
+                var uploadLocation = Path.Combine(Server.MapPath("~/Images"), filePath);
+                fileBase.SaveAs(uploadLocation);
+                p.Pictures = "/Images/" + filePath;
+            }
+            db.Table_Pictures.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+
+
+}
---
+++ Mvc-Images-Added/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derleme hakkındaki genel Bilgiler aşağıdakiler üzerinden denetlenir
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Mvc_Images_Added")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mvc_Images_Added")]
+[assembly: AssemblyCopyright("Telif hakkı ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özelliğinin false olarak ayarlanması bu derlemedeki türleri COM bileşenlerine
+//COM görünmez kılar.  Bu derlemedeki bir türe şu kaynaktan erişmeniz gerekiyorsa:
+// COM, bu tür üzerinde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("d7f52b16-9ab8-4d4e-b653-15b6d1f4cb4c")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya Gözden Geçirme ve Yapı Numaralarını varsayılan değerlerine döndürebilirsiniz
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
